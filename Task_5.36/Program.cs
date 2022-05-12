550f4f85ae84124307da45cc1222174ce72059a6
Orochi <orochi705@gmail.ru>
2022-05-12 21:08:47 +0900

    push

---
+++ Task_5.36/Program.cs
@@ -0,0 +1,18 @@
+﻿void print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] mass = new int[4];
+print(mass);
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-99, 99);
+}
+print(mass);
+var even = mass.Where((element, index) => index % 2 != 0);
+Console.WriteLine("Сумма на четных позициях: {1}.", even.Count(), even.Sum());
