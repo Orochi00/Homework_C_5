6d3d7195c8cbfbd63bf0344509b2da3f5b4d94be
Orochi <orochi705@gmail.ru>
2022-05-12 21:07:53 +0900

    push

---
+++ Task_5.34/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[,] arr = new int[1,4];
+Random rnd = new Random();
+for (int i = 0; i < arr.GetLength(0); i++)
+    for (int j = 0; j < arr.GetLength(1); j++)
+        arr[i, j] = rnd.Next(100, 999);
+for (int i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
+    for (int j = 0; j < arr.GetLength(1); j++)
+        Console.Write("{0,5}", arr[i, j]);
+int even;
+even = 0;
+for (int i = 0; i < arr.GetLength(0); i++)
+    for (int j = 0; j < arr.GetLength(1); j++)
+        if (arr[i, j] % 2 == 0)
+            even++;
+Console.WriteLine($"Количество четных: {even}");
